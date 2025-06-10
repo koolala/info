7706fb2371b7654d85e936cc8255bac3ebb51aa5
Koolala.cc <koolala.cc@gmail.com>
2025-06-10 10:51:38 +0800

    Update infocan-gen-form2pdf.js

--- infocan-gen-form2pdf.js
+++ infocan-gen-form2pdf.js
@@ -1,10 +1,6 @@
 import { html, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js'
-// import $ from 'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js';
-// import jsPDF from 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js';
-//import html2canvas from 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js';
 //import html2pdf from 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js';
-
-import html2pdf from 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js';
+import { html2pdf } from 'https://cdn.jsdelivr.net/gh/eKoopmans/html2pdf.js@0.10.3/dist/html2pdf.bundle.min.js';
 
 
 export class GenForm2PDF extends LitElement {
@@ -22,22 +18,21 @@ export class GenForm2PDF extends LitElement {
     }
 
     generatePDF() {
-        const element = $("div.nx-form.form");
-        var _self = this;
+        const _self = this;
+        const element = document.querySelector("div.nx-form.form");
 
         var opt = {
-                margin: _self.margin,
-                //filename:     'myfile.pdf',
-                image:        { type: 'jpeg', quality: 0.98 },
-                html2canvas:  { scale: 2 },
-                jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
+            margin: _self.margin,
+            //filename:     'myfile.pdf',
+            image:        { type: 'jpeg', quality: 0.98 },
+            html2canvas:  { scale: 2 },
+            jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
         };
 
 	    //# Test Mode
-        //html2pdf().set(opt).from(element[0]).save('my-pdf.pdf');
+        //html2pdf().set(opt).from(element).save('my-pdf.pdf');
 
-
-        html2pdf().set(opt).from(element[0]).outputPdf().then(function(pdf) {
+        html2pdf().set(opt).from(element).outputPdf().then(function(pdf) {
             // This logs the right base64
             //console.log(btoa(pdf));
             _self.pdfFileBase64 = btoa(pdf);
@@ -54,27 +49,27 @@ export class GenForm2PDF extends LitElement {
                     type: 'string',
                     title: 'PDF File Base64',
                     isValueField: true,
-                    required: true
+                    //required: true
                 },
                 margin: {
                     type: 'number',
                     title: 'PDF Margin',
                     minimum: 0,
                     maximum: 10,
-                    defaultValue: 0,
+                    defaultValue: 0
                 },
                 encryptPassword: {
                     type: 'string',
                     title: 'Encrypt PDF with Password',
                     description: 'If set, the PDF will be encrypted with this password.',
-                    defaultValue: '',
+                    defaultValue: ''
                 }
             }
         };
     }
 
     render() {
-        return html`<p><p/>`;
+        return html`<p>...</p>`;
     }
 }
 
